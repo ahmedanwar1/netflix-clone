bf19e946862685da2d9fc76904547e636010360c
Ahmed Anwar <ahmed.anwar444442@gmail.com>
2022-02-14 16:21:04 +0200

    minor changes

--- pages/api/movies/discover.ts
+++ pages/api/movies/discover.ts
@@ -6,12 +6,9 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "GET") {
-    console.log(req.query);
     const moviesList = await axios.get(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${req.query.genre_id}`
     );
-
-    // console.log(generesList);
 
     res.status(200).json(moviesList.data);
   } else {
