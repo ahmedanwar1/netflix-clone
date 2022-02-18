c878d10b92ee127f9c9714388212c2ba73895217
Ahmed Anwar <ahmed.anwar444442@gmail.com>
2022-02-19 00:00:24 +0200

    minor changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,16 @@ import styles from "../styles/Header.module.css";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import Avatar from "@mui/material/Avatar";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 const Header: React.FC = () => {
   const [darkBackground, setDarkBackground] = useState<boolean>(false);
+
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
 
   useEffect(() => {
     if (window !== undefined) {
@@ -41,16 +47,13 @@ const Header: React.FC = () => {
         </div>
         <div className={styles.right}>
           <SearchIcon />
-          {/* <Link href="/home">KIDS</Link>
-          <Link href="/home">DVD</Link> */}
           <NotificationsIcon />
-          {/* <div className={styles.profile}>
-          <img src="https://scontent.fcai21-3.fna.fbcdn.net/v/t39.30808-1/s320x320/273281161_3153911598161695_3361964561094897906_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=7206a8&_nc_ohc=ac_GwtuptygAX83_EBU&_nc_ht=scontent.fcai21-3.fna&oh=00_AT8ocPCniOr5YgsKnXSeyQu-K9tyPAkwHLfeF_ouZA3Y-w&oe=6206E509" />
-        </div> */}
           <Avatar
             alt="profile"
-            src="https://scontent.fcai21-3.fna.fbcdn.net/v/t39.30808-1/273281161_3153911598161695_3361964561094897906_n.jpg?stp=dst-jpg_s320x320&_nc_cat=105&ccb=1-5&_nc_sid=7206a8&_nc_ohc=iqokyTnYPvkAX-v9e9A&_nc_ht=scontent.fcai21-3.fna&oh=00_AT9kUkC6AGYg5KjCSseBP1MRufXmjjb-MB3SOuJ4KXSMMA&oe=62144429"
+            // src="https://scontent.fcai21-3.fna.fbcdn.net/v/t39.30808-1/273281161_3153911598161695_3361964561094897906_n.jpg?stp=dst-jpg_s320x320&_nc_cat=105&ccb=1-5&_nc_sid=7206a8&_nc_ohc=iqokyTnYPvkAX-v9e9A&_nc_ht=scontent.fcai21-3.fna&oh=00_AT9kUkC6AGYg5KjCSseBP1MRufXmjjb-MB3SOuJ4KXSMMA&oe=62144429"
+            src={session?.user?.image?.toString()}
             onClick={() => signOut()}
+            title="signout"
             style={{ cursor: "pointer" }}
           />
         </div>
